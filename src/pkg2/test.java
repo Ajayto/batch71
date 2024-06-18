fe16ce3480a2dd4153308744751e9ecc57e31399
Ajayto <ajaytoradmal2009@gmail.com>
2024-06-18 23:11:40 +0530

    updated test class in pkg 2

--- src/pkg2/test.java
+++ src/pkg2/test.java
@@ -5,6 +5,12 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//in side main method 
+		//in side main method 
+		//in side main method 
+		//in side main method 
+		
+
 	}
 
 }
