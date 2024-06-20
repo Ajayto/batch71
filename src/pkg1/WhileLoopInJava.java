b99186002ef018fed55aadc6ca399c8636c07ed3
Ajayto <ajaytoradmal2009@gmail.com>
2024-06-20 21:49:30 +0530

    Update WhileLoopInJava.java

--- src/pkg1/WhileLoopInJava.java
+++ src/pkg1/WhileLoopInJava.java
@@ -5,11 +5,11 @@ public class WhileLoopInJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// print number 0 to 10;
-		int number=1234;
+		int inputNumber=1234;
 		int count=0;
-		while(number>0) {
+		while(inputNumber>0) {
 			count++;  //2
-			number=number/10;  //123
+			inputNumber=inputNumber/10;  //123
 		}
 		System.out.println(count);
 
