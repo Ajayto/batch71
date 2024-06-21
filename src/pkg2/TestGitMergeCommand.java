dc5a8c9dc589dd2c97bd4547923a1e137924b92a
unknown <ajaytoradmal2009@gmail.com>
2024-06-21 21:57:02 +0530

    added file

---
+++ src/pkg2/TestGitMergeCommand.java
@@ -0,0 +1,12 @@
+package pkg2;
+
+public class TestGitMergeCommand {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("test class for git merge command");
+
+	}
+
+}
